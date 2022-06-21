238b8c0e7adb2efd4b55ccde16e887ce2a312686
erilu762@berkeley.edu <erilu762@berkeley.edu>
2022-06-20 23:15:05 -0700

    added queue system

--- music/play.js
+++ music/play.js
@@ -5,22 +5,66 @@ const {
 	createAudioResource,
 } = require('@discordjs/voice');
 const {AudioPlayerStatus } = require('@discordjs/voice');
-module.exports = async function (connection, value){
-	const stream = ytdl(value, { filter: 'audioonly' });
-    const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary, inlineVolume: true });
-    resource.volume.setVolume(0.5);
-	const player = createAudioPlayer();
-	connection.subscribe(player);
-	player.play(resource);
+module.exports = async function (connection, url, queue, guild, interaction){
+	ServerQueue = queue.get(guild)
+	if (!ServerQueue) {
+        const queueContruct = {
+          song: [],
+          volume: 0.5,
+          playing: true
+        };
+        queue.set(guild, queueContruct);
+		ServerQueue = queue.get(guild)
+		ServerQueue.song.push(url)
+		const stream = ytdl(url, { filter: 'audioonly' });
+    	const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary, inlineVolume: true });
+    	resource.volume.setVolume(0.5);//add a way to change volume
+		const player = createAudioPlayer();
+		connection.subscribe(player);
+		player.play(resource);
+		interaction.channel.send(`Playing ${ServerQueue.song[0].title}`);
 
-	player.on(AudioPlayerStatus.Playing, () => {
-		console.log('The audio player has started playing!');
-	});
-	player.on('error', error => {
-		console.error(`Error: ${error.message} with resource ${error.resource.metadata.title}`);
-		player.play(getNextResource());
-	});
-	player.on(AudioPlayerStatus.Idle, () => {
-		player.play(getNextResource());
-	});
+		player.on(AudioPlayerStatus.Playing, () => {
+			console.log('The audio player has started playing!');
+		});
+		player.on('error', error => {
+			console.error(`Error: ${error.message} with resource ${error.resource.metadata.title}`);
+			ServerQueue.song.shift()
+			if (!ServerQueue.song) {
+				queue.delete(guild.id);
+				return;
+			}
+			const stream = ytdl(ServerQueue.song[0].url, { filter: 'audioonly' });
+			const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary, inlineVolume: true });
+			resource.volume.setVolume(ServerQueue.volume);//add a way to change volume
+			const player = createAudioPlayer();
+			connection.subscribe(player);
+			player.play(resource);
+			interaction.channel.send(`Playing ${ServerQueue.song[0].title}`);
+			
+		});
+		player.on(AudioPlayerStatus.Idle, () => {
+			ServerQueue.song.shift()
+			if (!ServerQueue.song) {
+				queue.delete(guild.id);
+				return;
+			}
+			const stream = ytdl(ServerQueue.song[0].url, { filter: 'audioonly' });
+			const resource = createAudioResource(stream, { inputType: StreamType.Arbitrary, inlineVolume: true });
+			resource.volume.setVolume(ServerQueue.volume);//add a way to change volume
+			const player = createAudioPlayer();
+			connection.subscribe(player);
+			player.play(resource);
+			interaction.channel.send(`Playing ${ServerQueue.song[0].title}`);
+		});
+      }
+      else {
+		const songInfo = await ytdl.getInfo(url);
+		const song = {
+			title: songInfo.videoDetails.title,
+			url: songInfo.videoDetails.video_url,
+		};
+        ServerQueue.song.push(song);
+        return interaction.reply({ content: `playing ${song.title}`, ephemeral: false });
+    }
 }
--- music/search.js
+++ music/search.js
@@ -1,9 +1,9 @@
 const axios = require('axios');
 const play = require("./play.js");
-module.exports = async function (connection, keywords){
+module.exports = async function (connection, keywords, queue, guild, interaction){
         let url = `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=10&q=${keywords}&type=video&key=${process.env.YOUTUBE}`
         const res = await axios.get(url);
         //console.log(res.data.items[0].id.videoId);
         videoUrl = "https://www.youtube.com/watch?v=" + res.data.items[0].id.videoId;
-        play(connection, videoUrl)
+        play(connection, videoUrl, queue, guild, interaction);
 }
